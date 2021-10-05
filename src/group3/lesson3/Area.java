66a1fd890795ce82fd6bbaed487a919539a2785e
Gevor Mosesyan <movsesyang94@gmail.com>
2021-10-05 20:13:53 +0400

    New Group3

---
+++ src/group3/lesson3/Area.java
@@ -0,0 +1,16 @@
+package group3.lesson3;
+
+public class Area {
+
+    public static void main(String[] args) {
+
+        double pi, r, a;
+
+        r = 10.8;
+        pi = 3.416;
+        a = pi * r * r;
+
+        System.out.println("Плошадь круга равна " + "а");
+
+    }
+}

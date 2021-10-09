8e1f9d1ac3bd9eafc340db92c6ea88e2fa55db01
Gevor Mosesyan <movsesyang94@gmail.com>
2021-10-09 23:53:21 +0400

    Group3 tasks

---
+++ src/group3/lesson3/porc.java
@@ -0,0 +1,15 @@
+package group3.lesson3;
+
+public class porc{
+    public static void main(String[] args) {
+        int [] numbers = new int [5];
+        numbers [0]=31;
+        numbers [1]=34;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(numbers [i]);
+        }
+        }
+        }
+
+
+

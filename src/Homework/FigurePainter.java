ffbc913d18ce31e09bd0d1ff0914f63029d27c4c
Gevor Mosesyan <movsesyang94@gmail.com>
2021-10-12 23:27:35 +0400

    Homework

--- src/Homework/FigurePainter.java
+++ src/Homework/FigurePainter.java
@@ -49,6 +49,26 @@ public class FigurePainter{
             System.out.println();
 
         }
+
+
+        for (int i = 1; i <5 ; i++) {
+            for (int j = 3; j >=i ; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < 5 ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < 5-i ; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
 
